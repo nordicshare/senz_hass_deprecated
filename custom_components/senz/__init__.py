ca98bd99307bb707a5da775bc55f8bc6afdc1eb3
Åke Strandberg <ake@strandberg.eu>
2021-11-27 14:28:47 +0100

    Cleanup

--- custom_components/senz/__init__.py
+++ custom_components/senz/__init__.py
@@ -31,8 +31,6 @@ CONFIG_SCHEMA = vol.Schema(
     extra=vol.ALLOW_EXTRA,
 )
 
-# TODO List the platforms that you want to support.
-# For your initial PR, limit it to 1 platform.
 PLATFORMS = ["binary_sensor", "climate", "sensor"]
 
 
