79e27cbad10a34249d07bc9a19d63e63035b7927
Åke Strandberg <ake@strandberg.eu>
2022-01-28 21:46:11 +0100

    Set unique_id and handle multiple accounts

--- custom_components/senz/config_flow.py
+++ custom_components/senz/config_flow.py
@@ -11,6 +11,7 @@ from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers import config_entry_oauth2_flow
 
 from .const import DOMAIN
+from .pysenz import PreAPI
 
 
 class OAuth2FlowHandler(
@@ -36,9 +37,12 @@ class OAuth2FlowHandler(
         self, entry: dict[str, Any] | None = None
     ) -> FlowResult:
         """Perform reauth upon an API authentication error."""
+
+        self.entry = entry
+
         persistent_notification.async_create(
             self.hass,
-            f"Senz integration for account {entry['id']} needs to be re-authenticated. Please go to the integrations page to re-configure it.",
+            f"Senz integration for account {entry['auth_implementation']} needs to be re-authenticated. Please go to the [integrations page](/config/integrations) to re-configure it.",
             "Senz re-authentication",
             "senz_reauth",
         )
@@ -51,10 +55,24 @@ class OAuth2FlowHandler(
         if user_input is None:
             return self.async_show_form(
                 step_id="reauth_confirm",
-                description_placeholders={"account": self.entry["id"]},
+                description_placeholders={"account": self.entry["auth_implementation"]},
                 data_schema=vol.Schema({}),
                 errors={},
             )
 
         persistent_notification.async_dismiss(self.hass, "senz_reauth")
         return await self.async_step_user()
+
+    async def async_oauth_create_entry(self, data: dict) -> dict:
+        """Create an oauth config entry or update existing entry for reauth."""
+
+        pre_api = PreAPI(self.hass)
+        resp = await pre_api.getAccount(data["token"]["access_token"])
+        account = resp["userName"];
+
+        existing_entry = await self.async_set_unique_id(account)
+        if existing_entry:
+            self.hass.config_entries.async_update_entry(existing_entry, data=data)
+            await self.hass.config_entries.async_reload(existing_entry.entry_id)
+            return self.async_abort(reason="reauth_successful")
+        return self.async_create_entry(title=account, data=data)
